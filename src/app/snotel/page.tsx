7aeeaa97fc1f947df741000b2b149a40e5a9e7af
niedermansam <niederman.sam@gmail.com>
2023-05-07 10:27:52 -0600

    fix redirect

--- src/app/snotel/page.tsx
+++ src/app/snotel/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export default async function RedirectPage() {
+export default function RedirectPage() {
   const router = useRouter();
   
   const geohash = "c2qft";
 
+  useEffect(() => {
   router.push(
     `/snotel/near/${geohash}`
   );
-
+  }, [router, geohash]);
   return (
     <div>
       <h1>Redirecting...</h1>
