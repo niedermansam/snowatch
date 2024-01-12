06e799fa1234ced1402dc58aaf0bdf9bef5ac17b
Sam Supplee-Niederman <niederman.sam@gmail.com>
2024-01-12 15:36:27 -0700

    update snotel function

--- src/modules/snotel/server/getNearestSnotel.ts
+++ src/modules/snotel/server/getNearestSnotel.ts
@@ -1,21 +1,16 @@
 import { env } from "~/env.mjs";
 import { nearbySnotelMetadata } from "../types";
+import { nearestSnotelAPI } from "./nearestSnotelApi";
 
 export async function getNearestSnotel(
   geohash: string,
   n: number,
   minElevation = 0,
 ) {
-  const response = await fetch(
-    `${env.NEXT_PUBLIC_API_URL}/snotel/near/${geohash}/${n}?minElevation=${minElevation}`,
-    {
-      cache: "force-cache",
-    }
-  );
+  const response  = await nearestSnotelAPI(geohash, n, minElevation)
+  
 
-  const snotel = (await response.json()) as unknown;
-
-  const safeData = nearbySnotelMetadata.parse(snotel);
+  const safeData = nearbySnotelMetadata.parse(response);
 
   return safeData;
 }
